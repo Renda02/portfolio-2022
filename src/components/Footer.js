9e6f77c678c5d4597b4d5fbd23850afaa99d519a
Renda02 <rendaniluvhengo89@gmail.com>
2022-07-25 01:22:42 +0200

    creating Navigaion and adding styles

---
+++ src/components/Footer.js
@@ -0,0 +1,23 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <div>
+      {" "}
+      <ul className="Social links">
+        <li>
+          <a className="linkedIn" href="/">LinkedIn</a>
+        </li>
+        <li>
+          <a className="git" href="/">Git</a>
+        </li>
+        <li>
+          <a className="email" href="/">Email</a>
+        </li>
+       
+      </ul>
+    </div>
+  );
+}
+
+export default Footer;
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,11 +1,72 @@
-import React from 'react'
+import React from "react";
+import styled from "styled-components";
 
 function Nav() {
   return (
-    <div>
-        
-    </div>
-  )
+    <NavWrapper>
+      <Navigation>
+        <Logo className="logo-name">Rendani</Logo>
+        <NavMenu className="nav-list">
+          <NavItems>
+            <a href="/">Home</a>
+          </NavItems>
+          <NavItems>
+            <a href="/">Portfolio</a>
+          </NavItems>
+          <NavItems>
+            <a href="/">About Me</a>
+          </NavItems>
+        </NavMenu>
+      </Navigation>
+    </NavWrapper>
+  );
 }
 
-export default Nav
+export default Nav;
+
+const NavWrapper = styled.div`
+  position: sticky;
+  top: 0;
+  z-index: 5;
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  box-shadow: 0 1px 0 0 rgba(0, 0, 0, 0.12);
+`;
+
+const Logo = styled.h2`
+  font-size: 2em;
+  color: #5c31ac;
+`;
+
+const Navigation = styled.nav`
+  width: 100%;
+  padding: 1em 0;
+
+  display: flex;
+  justify-content: space-between;
+
+  z-index: 1;
+`;
+
+const NavMenu = styled.ul`
+  display: flex;
+
+  text-align: center;
+
+  @media screen and (max-width: 768px) {
+    display: none;
+  }
+`;
+
+const NavItems = styled.li`
+  justify-content: center;
+  align-items: center;
+  display: flex;
+  font-size: 18px;
+  text-decoration: none;
+  font-weight: 400;
+  padding: 10px;
+  text-align: center;
+  transition: all 0.3s ease;
+`;
